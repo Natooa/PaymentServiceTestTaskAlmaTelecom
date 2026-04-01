bcf81de481501ada005709f4a13a525b609321c0
Nurdiyar <nurdi.instagramm@gmail.com>
2026-04-01 23:07:36 +0500

    first commit init

---
+++ src/main/java/kz/natooa/paymentservice/PaymentServiceTestTaskAlmaTelecomApplication.java
@@ -0,0 +1,13 @@
+package kz.natooa.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceTestTaskAlmaTelecomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServiceTestTaskAlmaTelecomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/natooa/paymentservice/PaymentServiceTestTaskAlmaTelecomApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.natooa.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceTestTaskAlmaTelecomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
